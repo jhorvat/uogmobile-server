c37ffd97a8733a871527f7f66927b8e25a001304
Julian Horvat <julian.m.horvat@gmail.com>
2016-01-11 12:52:34 -0500

    successfully switched to selenium

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
+from app.web_advisor.module import mod as web_advisor_module
 
 app = Flask(__name__)
-
-from app.web_advisor.module import mod as web_advisor_module
 app.register_blueprint(web_advisor_module)
