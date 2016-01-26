1f7c47f214154ca19a6057aa5c730bbb85d3dd5f
Julian Horvat <julian.m.horvat@gmail.com>
2016-01-25 19:27:54 -0500

    successfully stealing sessions

--- app/web_advisor/constants.py
+++ app/web_advisor/constants.py
@@ -1,1 +1,71 @@
 JS_SCRIPTS = {}
+
+# Store this as a function so that we always get a fresh template of the cookie payload
+WEB_ADVISOR_COOKIES_TEMPLATE = lambda: {
+    "token": {
+        "name": None,
+        "value": None,
+        "path": "/WebAdvisor",
+        "domain": "webadvisor.uoguelph.ca"
+    },
+    "LASTTOKEN": {
+        "name": "LASTTOKEN",
+        "value": None,
+        "path": "/WebAdvisor",
+        "domain": "webadvisor.uoguelph.ca"
+    },
+    "__utma": {
+        "name": "__utma",
+        "value": None,
+        "path": "/WebAdvisor",
+        "domain": "webadvisor.uoguelph.ca"
+    },
+    "__utma_prime": {
+        "name": "__utma",
+        "value": None,
+        "path": "/",
+        "domain": ".webadvisor.uoguelph.ca"
+    },
+    "__utmb": {
+        "name": "__utmb",
+        "value": None,
+        "path": "/WebAdvisor",
+        "domain": "webadvisor.uoguelph.ca"
+    },
+    "__utmb_prime": {
+        "name": "__utmb",
+        "value": None,
+        "path": "/",
+        "domain": ".webadvisor.uoguelph.ca"
+    },
+    "__utmc": {
+        "name": "__utmc",
+        "value": None,
+        "path": "/WebAdvisor",
+        "domain": "webadvisor.uoguelph.ca"
+    },
+    "__utmc_prime": {
+        "name": "__utmc",
+        "value": None,
+        "path": "/",
+        "domain": ".webadvisor.uoguelph.ca"
+    },
+    "__utmz": {
+        "name": "__utmz",
+        "value": None,
+        "path": "/WebAdvisor",
+        "domain": "webadvisor.uoguelph.ca"
+    },
+    "__utmz_prime": {
+        "name": "__utmz",
+        "value": None,
+        "path": "/",
+        "domain": ".webadvisor.uoguelph.ca"
+    },
+    "survey": {
+        "name": "survey",
+        "value": None,
+        "path": "/WebAdvisor",
+        "domain": "webadvisor.uoguelph.ca"
+    }
+}
--- app/web_advisor/module.py
+++ app/web_advisor/module.py
@@ -1,6 +1,6 @@
 import io
 from functools import wraps
-from flask import Blueprint, request, send_file, g, abort
+from flask import Blueprint, request, session, send_file, g, abort
 from flask import current_app as app
 
 
@@ -29,21 +29,55 @@ def requires_login(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         """
-        Fill the login form before moving on to the actual request
+        Get the WebAdvisor cookie payload
         """
+        cookie_payload = constants.WEB_ADVISOR_COOKIES_TEMPLATE()
         wd = g.get("wd", None)
 
         if not wd:
             abort(500)
 
-        wd.get("https://webadvisor.uoguelph.ca/WebAdvisor/WebAdvisor?CONSTITUENCY=WBDF&type=P&pid=UT-LGRQ&PROCESS=-UTAUTH01") # Login to WebAdvisor
-        wd.find_elements_by_selector("#USER_NAME").send_keys(app.config["USER_NAME"])
-        wd.find_elements_by_selector("#CURR_PWD").send_keys(app.config["PASSWORD"])
-        wd.find_elements_by_selector("#content > div.screen.UTAUTH01 > form").submit()
-        print "Logged in"
+        if "cookies" not in session:
+            abort(403)
+
+        wd.get("https://webadvisor.uoguelph.ca/WebAdvisor/WebAdvisor?CONSTITUENCY=WBDF&type=P&pid=UT-LGRQ&PROCESS=-UTAUTH01")
+        for cookie in wd.get_cookies():
+            """
+            WebAdvisor sets a unique name for the session cookie based on the machine so we MUST preserve that, however the session
+            info itself isn't cross-referenced so as long as the cookie name and value match indepently we're good
+            """
+            if cookie["name"].isdigit():
+                cookie_payload["token"]["name"] = cookie["name"]
+
+        for name, value in session["cookies"]:
+            if name.startswith("__"):
+                if name == "__utmb":
+                    cookie_payload["__utmb"]["value"] = value
+                    cookie_payload["__utmb_prime"]["value"] = value
+                elif value.endswith("**"):
+                    cookie_payload[name]["value"] = value
+                else:
+                    cookie_payload[name + "_prime"]["value"] = value
+            elif name not in cookie_payload:
+                cookie_payload["token"]["value"] = value
+            else:
+                cookie_payload[name]["value"] = value
+
+        wd.delete_all_cookies()
+        map(lambda (k, v): wd.add_cookie(v), cookie_payload.iteritems())
 
         return f(*args, **kwargs)
     return decorated_function
+
+@mod.route("/login", methods=['POST'])
+def login():
+    data = request.get_json()
+
+    if not data["cookie"]:
+        abort(400)
+
+    session["cookies"] = [cookie.split("=", 1) for cookie in data["cookie"].replace(" ", "").split(";")]
+    return "Success"
 
 @mod.route("/schedule", methods=['GET'])
 @requires_login
@@ -58,10 +92,8 @@ def schedule():
         abort(500)
 
     wd.get("https://webadvisor.uoguelph.ca/WebAdvisor/WebAdvisor?CONSTITUENCY=WBST&type=P&pid=ST-WESTS13A") # Select the current semester
-    print "Fetched term select"
-    wd.find_elements_by_selector("#VAR4").select_by_value("W16")
-    wd.find_elements_by_selector("#content > div.screen.WESTS13A > form").submit()
-
-    return to_json(wd.execute_script(constants.JS_SCRIPTS["class_schedule_extractor"])) # Run the parser script on the page and return the script's result
-
-    # return send_file(io.BytesIO(wd.get_screenshot_as_png()), attachment_filename='logo.png', mimetype='image/png')
+    return send_file(io.BytesIO(wd.get_screenshot_as_png()), attachment_filename='logo.png', mimetype='image/png')
+    # wd.find_elements_by_selector("#VAR4").select_by_value("W16")
+    # wd.find_elements_by_selector("#content > div.screen.WESTS13A > form").submit()
+    #
+    # return to_json(wd.execute_script(constants.JS_SCRIPTS["class_schedule_extractor"])) # Run the parser script on the page and return the script's result
