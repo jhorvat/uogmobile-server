ea1cab9b4a252ab6c906b6eaf144aa042ab9cf32
Julian Horvat <julian.m.horvat@gmail.com>
2016-02-05 14:44:37 -0500

    Refactore navigator into context manager

--- app/web_advisor/module.py
+++ app/web_advisor/module.py
@@ -1,22 +1,13 @@
 import io
 from functools import wraps
-from flask import Blueprint, request, session, send_file, g, abort, jsonify
+from flask import Blueprint, request, session, send_file, abort, jsonify
 from flask import current_app as app
 
 from . import constants
-from app.utils import to_json
 from .navigator import Navigator
 
 # TODO: Migrate to WebDriver to context manager maybe
 mod = Blueprint('web_advisor', __name__, url_prefix="/webadvisor")
-
-def requires_navigator(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        g.wd = Navigator()
-        return f(*args, **kwargs)
-    return decorated_function
-
 
 def requires_login(f):
     @wraps(f)
@@ -24,15 +15,8 @@ def requires_login(f):
         """
         Get the WebAdvisor cookie payload
         """
-        wd = g.get("wd", None)
-
-        if not wd:
-            abort(500)
-
         if "cookies" not in session:
             abort(403)
-
-        wd.inject_session(session["cookies"])
 
         return f(*args, **kwargs)
     return decorated_function
@@ -68,30 +52,27 @@ def login():
     return jsonify({})
 
 @mod.route("/schedule", methods=['GET'])
-@requires_navigator
 @requires_login
 def schedule():
     """
     /webadvisor/schedule
     Gets the current semester's schedule from WebAdvisor
     """
-    try:
-        wd = g.get("wd", None)
+    schedule = None
 
+    with Navigator() as wd:
+        wd.inject_session(session["cookies"])
         wd.class_schedule()
         wd.find_elements_by_selector("#VAR4").select_by_value("W16")
         wd.find_elements_by_selector("#content > div.screen.WESTS13A > form").submit()
 
         schedule = wd.execute_script(constants.JS_SCRIPTS["class_schedule_extractor"])
-    except:
-        wd.get_screenshot_as_file("error.png")
-    finally:
-        wd.quit()
 
-        if schedule:
-            return jsonify(schedule) # Run the parser script on the page and return the script's result
-        else:
-            abort(500)
+    if schedule:
+        return jsonify(schedule) # Run the parser script on the page and return the script's result
+    else:
+        abort(500)
+
     # return send_file(io.BytesIO(wd.get_screenshot_as_png()), attachment_filename='logo.png', mimetype='image/png')
 
 @mod.route("/error", methods=['GET'])
--- app/web_advisor/navigator.py
+++ app/web_advisor/navigator.py
@@ -1,6 +1,10 @@
-from app.utils import PhantomDriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
 
-class Navigator(PhantomDriver):
+class Navigator(webdriver.Remote):
     """
     PhantomDriver wrapper class to abstract away WebAdvisor URLs and other nitty-gritty like injecting the session
     """
@@ -12,8 +16,20 @@ class Navigator(PhantomDriver):
         """
         Super init and then navigate to the login page since we always want there to be a page immediately
         """
-        super(Navigator, self).__init__()
+        super(Navigator, self).__init__(command_executor='http://localhost:4444/wd/hub', desired_capabilities=DesiredCapabilities.PHANTOMJS)
+
+    def __enter__(self):
         self.login_page()
+        WebDriverWait(self, 30, poll_frequency=0.1).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#content > div.screen.UTAUTH01 > form"))
+        )
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is not None:
+            print("Driver exited with error!")
+
+        self.quit()
 
     def login_page(self):
         self.get(self._login_url)
@@ -41,3 +57,18 @@ class Navigator(PhantomDriver):
                 self.add_cookie(cookie)
             except:
                 print("Failed to set cookie\n" + str(cookie))
+
+    def find_elements_by_selector(self, selector):
+        """
+        Wrapper for find_elements_by_css_selector.
+
+        :param selector: Valid CSS selector for the element
+        :return: Returns either a list of elements, or if only one exists the single element. If any elements are <select> tags they are converted to Selenium Select objects
+        """
+
+        elements = [el if el.tag_name != "select" else Select(el) for el in self.find_elements_by_css_selector(selector)] # Convert any <select> tags
+        if len(elements) == 1:
+            return elements[0]
+
+        print(elements)
+        return elements
